339f065b0ff074a0d729a89bf3cfddf5aa341d98
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2024-07-29 16:44:23 +0530

    host change

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['cineplex-dlbe.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cineplex-87ag.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     

802e2a42d3e6a6becb057b7c0602a34b57e58af4
Eldhose Iype <eldhoseiypeparavatty@gmail.com>
2024-07-29 12:11:03 +0530

    database change

--- mini_project/settings.py
+++ mini_project/settings.py
@@ -68,8 +68,12 @@ WSGI_APPLICATION = 'mini_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
         'OPTIONS': {
             'timeout': 20,  
         }
